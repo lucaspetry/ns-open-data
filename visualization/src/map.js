55ab41480ac865d8eefb4704ea57974ca91b9298
MateusMP <mateus.mp93@gmail.com>
2019-03-02 14:08:06 -0400

    Beggining pie chart

--- visualization/src/map.js
+++ visualization/src/map.js
@@ -91,6 +91,11 @@ function fetchDatasetValue(d, data, variable) {
     return data[countyName(d)][variable];
 }
 
+// Assuming: data { county: value, ... }
+function updateMapWithIndex(data) {
+    updateMap(data, 'value');
+}
+
 // Update map colors using given data and variable
 function updateMap(data, variable, tooltipHtml) {
     
@@ -152,3 +157,51 @@ d3.json('data/map10.geojson').then(function (geojson) {
 
     theMap.elements = mapElements;
 });
+
+
+// Pie chart
+data = {
+    "A": 5,
+    "B": 10,
+    "C": 20
+};
+
+chart = {
+    const arcs = pie(data);
+  
+    const svg = d3.select(DOM.svg(width, height))
+        .attr("text-anchor", "middle")
+        .style("font", "12px sans-serif");
+  
+    const g = svg.append("g")
+        .attr("transform", `translate(${width / 2},${height / 2})`);
+    
+    g.selectAll("path")
+      .data(arcs)
+      .enter().append("path")
+        .attr("fill", d => color(d.data.name))
+        .attr("stroke", "white")
+        .attr("d", arc)
+      .append("title")
+        .text(d => `${d.data.name}: ${d.data.value.toLocaleString()}`);
+  
+    const text = g.selectAll("text")
+      .data(arcs)
+      .enter().append("text")
+        .attr("transform", d => `translate(${arcLabel.centroid(d)})`)
+        .attr("dy", "0.35em");
+    
+    text.append("tspan")
+        .attr("x", 0)
+        .attr("y", "-0.7em")
+        .style("font-weight", "bold")
+        .text(d => d.data.name);
+    
+    text.filter(d => (d.endAngle - d.startAngle) > 0.25).append("tspan")
+        .attr("x", 0)
+        .attr("y", "0.7em")
+        .attr("fill-opacity", 0.7)
+        .text(d => d.data.value.toLocaleString());
+  
+    return svg.node();
+  }
